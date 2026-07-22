fb4f0c001a6770a99fef99bce2dadd86e590bc2a
Lucas Alberto <lucas.a.santos126@aluno.senai.br>
2026-07-22 14:18:26 -0300

    compilação do arquivo app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
-"use strict";
-let message = "Olá mundo";
-console.log(message);
+const alunos = [];
+const formAluno = document.querySelector('#form-aluno');
+formAluno?.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    const dadosForm = new FormData(formAluno);
+    const aluno = {
+        nome: dadosForm.get('nome-aluno'),
+        datanascimento: dadosForm.get('nascimento-add'),
+        sexo: dadosForm.get('sexo-add'),
+        ano: dadosForm.get('ano-add'),
+        turma: dadosForm.get('turma-add'),
+        matricula: dadosForm.get('matricula-add'),
+        nota01: Number(dadosForm.get('nota01-add')),
+        nota02: Number(dadosForm.get('nota02-add')),
+        nota03: Number(dadosForm.get('nota03-add')),
+        media: (Number(dadosForm.get('nota01-add')) + Number(dadosForm.get('nota02-add')) + Number(dadosForm.get('nota03-add'))) / 3
+    };
+    alunos.push(aluno);
+    console.log(aluno);
+    formAluno?.reset();
+});
+//# sourceMappingURL=app.js.map
