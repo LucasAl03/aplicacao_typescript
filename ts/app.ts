81087073fda67eaa729ab9d7b28d520393eb8244
Lucas Alberto <lucas.a.santos126@aluno.senai.br>
2026-07-22 19:58:22 -0300

    adicionado funcao para calcular idade do aluno

--- ts/app.ts
+++ ts/app.ts
@@ -13,16 +13,46 @@ interface Aluno {
     nota03: number
 }
 
+function CalcularIdade(nasciData) {
+    var nascimentoData = new Date(nasciData)
+
+    var nascimentoDataDia = nascimentoData.getDate();
+    var nascimentoDataMes = nascimentoData.getMonth();
+    var nascimentoDataAno = nascimentoData.getFullYear();
+
+    var dataHoje = new Date();
+    var diaHoje = dataHoje.getDate();
+    var mesHoje = dataHoje.getMonth();
+    var anoHoje = dataHoje.getFullYear();
+
+    var idade = 0;
+
+    if (mesHoje > nascimentoDataMes) {
+        idade = anoHoje - nascimentoDataAno;
+    } else if (mesHoje === nascimentoDataMes) {
+        idade = diaHoje >= nascimentoDataDia ? anoHoje - nascimentoDataAno : anoHoje - nascimentoDataAno - 1;
+    } else {
+        idade = anoHoje - nascimentoDataAno - 1;
+    }
+
+    return idade;
+}
+
 const formAluno = document.querySelector<HTMLFormElement>('#form-aluno')
+const cardsLista = document.querySelector('#cards-lista')
+const cardAluno = document.querySelector('#card-aluno')
 
 formAluno?.addEventListener('submit', (evt: Event)=>{
     evt.preventDefault()
 
     const dadosForm = new FormData(formAluno)
+    
+    const nasciData = dadosForm.get('nascimento-add');
 
     const aluno = {
         nome: dadosForm.get('nome-aluno'),
         datanascimento: dadosForm.get('nascimento-add'),
+        idade: CalcularIdade(nasciData),
         sexo: dadosForm.get('sexo-add'),
         ano: dadosForm.get('ano-add'),
         turma: dadosForm.get('turma-add'),
@@ -33,10 +63,24 @@ formAluno?.addEventListener('submit', (evt: Event)=>{
         media: (Number(dadosForm.get('nota01-add')) + Number(dadosForm.get('nota02-add')) + Number(dadosForm.get('nota03-add'))) / 3
     }
     
-    alunos.push(aluno)
+    addAluno(aluno)
 
     console.log(aluno)
 
     formAluno?.reset() 
 })
 
+const addAluno = (objAluno)=>{
+    alunos.push(objAluno)
+
+    listAluno()
+}
+
+const listAluno = () => {
+    cardsLista?.innerHTML = ''
+    
+
+    obj.forEach((elem, i) => {
+        cardAluno.innerHTML += `` 
+    })
+}
