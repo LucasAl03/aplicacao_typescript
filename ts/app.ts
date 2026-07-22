f564b7bcc394a87f7b640186eb1e44dc9ebb1c0c
Lucas Alberto <lucas.a.santos126@aluno.senai.br>
2026-07-21 21:31:40 -0300

    codigo para ver a array de informações adicionadas no formulario

--- ts/app.ts
+++ ts/app.ts
@@ -1,9 +1,30 @@
-const alunos = []
 
-const formAluno = document.querySelector('#form-aluno')
+//const alunos: any = []
 
-formAluno.addEventListener('submit', (evt)=>{
+const formAluno: any = document.querySelector('#form-aluno')
+const 
+
+formAluno?.addEventListener('submit', (evt: Event)=>{
     evt.preventDefault()
 
-    formAluno.reset()
+    const dadosForm = new FormData(formAluno)
+
+    const aluno = {
+        nome: dadosForm.get('nome-aluno'),
+        datanascimento: dadosForm.get('nascimento-add'),
+        sexo: dadosForm.get('sexo-add'),
+        ano: dadosForm.get('ano-add'),
+        turma: dadosForm.get('turma-add'),
+        matricula: dadosForm.get('matricula-add'),
+        nota01: dadosForm.get('nota01-add'),
+        nota02: dadosForm.get('nota02-add'),
+        nota03: dadosForm.get('nota03-add'),
+    }
+
+    console.log(aluno)
+
+    
+
+    formAluno.reset() 
 })
+
